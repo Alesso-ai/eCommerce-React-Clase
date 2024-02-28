93ce8a6bd3e79a453b6c07b846b97aa4a49c8627
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2024-02-28 09:18:42 +0100

    Proyecto react

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { API_URL } from "../constants/env"
+
+function App(){
+  return (
+    <div>
+      <h1>José Luis Ábalos Putero <br />{API_URL}</h1>
+    </div>
+  )
+}
+
+export default App
